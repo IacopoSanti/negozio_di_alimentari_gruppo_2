aeb7009807a64392e8a920c9f01fe6da0db38087
Iacopo Santi <iacopo.santi@studenti.isissgobetti.it>
2023-03-27 13:54:04 +0200

    Classe Cliente

---
+++ Cliente.js
@@ -0,0 +1,10 @@
+class Cliente {
+    constructor(nome, cognome, id, saldo_disponibile) {
+        this.nome = nome;
+        this.cognome = cognome;
+        this.id = id;
+        this.saldo_disponibile = saldo_disponibile;
+    }
+
+    
+}
